5d6134f83f3db3e7ce6a9d4da907ad31cb410a42
Hendrik.Ebbers@web.de <Hendrik.Ebbers@web.de@17bb0ecf-38c4-95f9-1e6d-ca9926013565>
2011-06-15 21:33:53 +0000

    Maven New Demos Renderer

---
+++ branches/0.2/JGrid/src/main/java/de/jgrid/renderer/DefaultGridCellRenderer.java
@@ -0,0 +1,179 @@
+/*
+ * Created on Jan 22, 2011
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2011 Hendrik Ebbers
+ */
+package de.jgrid.renderer;
+
+import java.awt.Component;
+import java.awt.Rectangle;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+import de.jgrid.JGrid;
+
+/**
+ * The default CellRenderer for JGrid. Works like DefaultListRenderer.
+ * 
+ * @author Hendrik Ebbers
+ * @since 0.1
+ * @version 0.1
+ * @see DefaultListRenderer
+ */
+public class DefaultGridCellRenderer extends JLabel implements GridCellRenderer {
+
+	private static final long serialVersionUID = 1L;
+
+	public DefaultGridCellRenderer() {
+		setHorizontalTextPosition(CENTER);
+		setVerticalTextPosition(CENTER);
+		setHorizontalAlignment(CENTER);
+		setVerticalAlignment(CENTER);
+	}
+
+	@Override
+	public Component getGridCellRendererComponent(JGrid grid, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+
+		if (isSelected) {
+			setBackground(grid.getSelectionBackground());
+			setForeground(grid.getSelectionForeground());
+		} else {
+			setBackground(grid.getBackground());
+			setForeground(grid.getForeground());
+		}
+
+		if (value instanceof Icon) {
+			setIcon((Icon) value);
+			setText("");
+		} else {
+			setIcon(null);
+			setText((value == null) ? "" : value.toString());
+		}
+
+		setEnabled(grid.isEnabled());
+		setFont(grid.getFont());
+		return this;
+	}
+
+	@Override
+	public String getToolTipText() {
+		return getText();
+	}
+	
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void validate() {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void invalidate() {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void repaint() {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void revalidate() {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void repaint(long tm, int x, int y, int width, int height) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void repaint(Rectangle r) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, byte oldValue,
+			byte newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, char oldValue,
+			char newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, short oldValue,
+			short newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, int oldValue,
+			int newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, long oldValue,
+			long newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, float oldValue,
+			float newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, double oldValue,
+			double newValue) {
+	}
+
+	/**
+	 * Overridden for performance reasons.
+	 */
+	@Override
+	public void firePropertyChange(String propertyName, boolean oldValue,
+			boolean newValue) {
+	}
+}
